5d746e6457472d0e49e4f092d90dc7ba32944568
Mads Jensen <madsj20@student.sdu.dk>
2025-03-20 10:37:07 +0100

    Started prototype for GameManager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,13 +4,43 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    private int challange = 0;
+    private int challangesCompleted = 0;
+
     private void Start()
     {
-        
+        StartCoroutine(ChooseChallange());
     }
 
     private void Update()
     {
-        
+
+    }
+
+    private IEnumerator ChooseChallange()
+    {
+        challange = Random.Range(1, 4);
+        switch (challange)
+        {
+            case 1:
+                print("1");
+                yield return new WaitForSeconds(3);
+                StartCoroutine(ChooseChallange());
+                break;
+            case 2:
+                print("2");
+                yield return new WaitForSeconds(3);
+                StartCoroutine(ChooseChallange());
+                break;
+            case 3:
+                print("3");
+                yield return new WaitForSeconds(3);
+                StartCoroutine(ChooseChallange());
+                break;
+            default:
+                print("Incorrect intelligence level.");
+                StartCoroutine(ChooseChallange());
+                break;
+        }
     }
 }
